54cfa400b4fc634ee7ae610d310caaab02a2932c
Anfisa <midlovetsanfisa@gmail.com>
2024-05-01 02:13:15 +0300

    test

--- test1.cpp
+++ test1.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+int factorial(int n) {
+
+    if (n == 0) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
 int main() {
-    
+    int number;
+    cout << "Введіть ціле число: ";
+    cin >> number;
+
+
+    if (number < 0) {
+        cout << "Факторіал не визначений для негативних чисел" << endl;
+    } else {
+        int result = factorial(number);
+        cout << "Факторіал " << number << " дорівнює: " << result << endl;
+    }
+
+    return 0;
 }

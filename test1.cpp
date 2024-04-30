8c10d9a4d76984be5945e1f0e736c48ea095c529
Valleriiia <valeria.nepomiashcha@gmail.com>
2024-04-30 13:40:20 +0300

    read from file

--- test1.cpp
+++ test1.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 struct treeNode {
     string text;
     treeNode *yes, *no;
 };
+
+void readFromFile(treeNode* &root) {
+    ifstream file("animals_tree.txt");
+    string input;
+    getline(file, input);
+    if (input == "#") {
+        root = NULL;
+        return;
+    }
+    root = new treeNode();
+    root->text = input;
+    readFromFile(root->yes);
+    readFromFile(root->no);
+}

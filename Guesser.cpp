53af48bbffb6d2e46bc7e08f8910f1ef0207903a
Valleriiia <valeria.nepomiashcha@gmail.com>
2024-06-07 01:47:32 +0300

    Guesser comments

--- Guesser.cpp
+++ Guesser.cpp
@@ -1,6 +1,8 @@
 #include "Guesser.h"
 
+// Обробляє припущення користувача
 bool Guesser::guess(const string& animal, Language lang) {
+    // Просить користувача ввести своє припущення
     if (lang == ukr) {
         cout << "\nВаше припущення: ";
     } else {
@@ -9,11 +11,14 @@ bool Guesser::guess(const string& animal, Language lang) {
     string guess;
     getline(cin, guess);
 
+    // Переводить ввід користувача та зага дану тварину у нижній регістр
     userInput::stringToLower(guess);
     string animLow = animal;
     userInput::stringToLower(animLow);
 
+    // Перевіряє чи припущення користувача збігається з загаданою твариною
     if (guess == animLow) {
+        // Виводить відповідь
         if (lang == ukr) {
             cout << "\nВітаю! Ви вгадали тварину: " << animal << "!" << endl;
         } else {
